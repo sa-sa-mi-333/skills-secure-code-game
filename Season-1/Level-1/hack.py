6daa326fecfbd5dfe32c84b78eaaa3f062186408
sasami_linuc <aym12a4005@icloud.com>
2025-06-11 00:42:20 +0900

    update Level-1 hack

--- Season-1/Level-1/hack.py
+++ Season-1/Level-1/hack.py
@@ -5,7 +5,7 @@ class TestOnlineStore(unittest.TestCase):
 
     # Tricks the system and walks away with 1 television, despite valid payment & reimbursement
     def test_6(self):
-        tv_item = c.Item(type='product', description='tv', amount=1000.00, quantity=1)
+        tv_item = c.Item(type='product', description='tv', amount=1000.0000000000000000000000, quantity=1)
         payment = c.Item(type='payment', description='invoice_4', amount=1e19, quantity=1)
         payback = c.Item(type='payment', description='payback_4', amount=-1e19, quantity=1)
         order_4 = c.Order(id='4', items=[payment, tv_item, payback])
